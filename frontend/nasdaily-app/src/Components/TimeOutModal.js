8e1c01d33b8c3431d91fe202dcf30ac56254b1e4
Andre Enriquez <andre.enriquez@cit.edu>
2023-05-30 00:52:58 +0800

    Add styles to modals

--- frontend/nasdaily-app/src/Components/TimeOutModal.js
+++ frontend/nasdaily-app/src/Components/TimeOutModal.js
@@ -1,5 +1,7 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import './TimeOutModal.css'
 
 function TimeOutModal(props) {
   return (
@@ -10,23 +12,46 @@ function TimeOutModal(props) {
         aria-labelledby="contained-modal-title-vcenter"
         centered
     >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Non-Academic Scholar
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Summary of Daily Activities</h4>
-        <p>
-          Activities of the Week
-          Skills Learned
-          Values Learned
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Done</Button>
-      </Modal.Footer>
-    </Modal>
+      <div className='timeoutmodal-container'>
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            NAS Daily Activity
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Activities Done</Form.Label>
+              <Form.Control as="textarea" rows={3} className="text-area"/>
+            </Form.Group>
+          </Form>
+          <Form>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Skills Learned</Form.Label>
+              <Form.Control as="textarea" rows={3} className="text-area"/>
+            </Form.Group>
+          </Form>
+          <Form>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Values Learned</Form.Label>
+              <Form.Control as="textarea" rows={3} className="text-area"/>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide} className='timeoutmodal-btn'>Done</Button>
+        </Modal.Footer>
+        </div>
+      </Modal>
   );
 }
 
