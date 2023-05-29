4fc42262eb273b3432f0c40ff7c7328abb707bad
Andre Enriquez <andre.enriquez@cit.edu>
2023-05-29 23:22:51 +0800

    Add modals for time in/out and welcome msg

--- frontend/nasdaily-app/src/Pages/Timeinout.js
+++ frontend/nasdaily-app/src/Pages/Timeinout.js
@@ -2,15 +2,24 @@ import React, { useState } from 'react';
 import './Timeinout.css';
 import Header from '../Components/Header';
 import GLE from '../images/glebuilding.png';
+import TimeInModal from '../Components/TimeInModal';
+import TimeOutModal from '../Components/TimeOutModal'
+import Welcome from '../Components/Welcome';
 
 function Timeinout() {
   const [buttonText, setButtonText] = useState('Time In');
+  const [timeInModalVisible, setTimeInModalVisible] = useState(false);
+  const [timeOutModalVisible, setTimeOutModalVisible] = useState(false);
 
   const handleClick = () => {
     if (buttonText === 'Time In') {
       setButtonText('Time Out');
+      setTimeInModalVisible(true); // Show the modal
+      setTimeOutModalVisible(false);
     } else {
       setButtonText('Time In');
+      setTimeInModalVisible(false); // Hide the modal
+      setTimeOutModalVisible(true);
     }
   };
 
@@ -19,10 +28,23 @@ function Timeinout() {
       <Header username="USERNAME" />
       <div className="timeinout-image-container">
         <img src={GLE} alt="GLE" className="image-size" />
+        <Welcome className="welcome"/>
         <button className="button" onClick={handleClick}>
           {buttonText}
         </button>
       </div>
+      {timeInModalVisible && (
+        <TimeInModal
+          show={timeInModalVisible}
+          onHide={() => setTimeInModalVisible(false)}
+        />
+      )}
+      {timeOutModalVisible && (
+        <TimeOutModal
+          show={timeOutModalVisible}
+          onHide={() => setTimeOutModalVisible(false)}
+        />
+      )}
     </div>
   );
 }
