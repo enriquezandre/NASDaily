68315c4f1d555e936394d8dee2cd63a88ef3f68c
Andre Enriquez <andre.enriquez@cit.edu>
2023-05-29 19:33:26 +0800

    Add img initial position

--- frontend/nasdaily-app/src/Pages/Timeinout.js
+++ frontend/nasdaily-app/src/Pages/Timeinout.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import './Timeinout.css'
 import Header from '../Components/Header'
+import GLE from '../images/glebuilding.png'
 
 function Timeinout() {
   return (
     <div>
         <Header username="Kaye Cassandra Belderol" />
+        <div className="image-container">
+            <img src={GLE} alt='GLE' className="image-size"/>
+        </div>
     </div>
   )
 }
