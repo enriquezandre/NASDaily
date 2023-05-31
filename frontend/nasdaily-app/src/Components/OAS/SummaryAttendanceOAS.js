9bbdb1c3cb501df670f064da274ef895344d300e
Stella Mariz Palen <stellamariz.palen@cit.edu>
2023-05-31 23:45:01 +0800

    Change to Class Component

---
+++ frontend/nasdaily-app/src/Components/OAS/SummaryAttendanceOAS.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import WeeklySummaryAttendance from './WeeklySummaryAttendance';
+import './SummaryAttendanceOAS.css';
+
+class SummaryAttendanceOAS extends Component {
+  render() {
+    const { check, totalLates, unexcusedAbsent, excusedAbsent, tenMinLate, fortyFiveMinLate, ftp } = this.props;
+
+    const attend = [
+      {
+        date: '05/30/2023',
+        timein: '08:00 AM',
+        timeout: '08:00 AM',
+        actsDone: 'Dummy Text',
+        skillsLearned: 'Dummy Text',
+        valuesLearned: 'Dummy Text',
+      },
+      {
+        date: '05/30/2023',
+        timein: '08:00 AM',
+        timeout: '08:00 AM',
+        actsDone: 'Dummy Text',
+        skillsLearned: 'Dummy Text',
+        valuesLearned: 'Dummy Text',
+      },
+    ];
+
+    return (
+      <>
+        {check ? (
+          <div>
+            <div className="weeklySummaryOfAttendance">
+              <h4 className="title">WEEKLY ATTENDANCE</h4>
+              <WeeklySummaryAttendance attend={attend} />
+            </div>
+
+            <br />
+          </div>
+        ) : (
+          <p className="errorMessage">No information to display. Please click on the search bar to access information of a NAS.</p>
+        )}
+      </>
+    );
+  }
+}
+
+export default SummaryAttendanceOAS;
--- frontend/nasdaily-app/src/Pages/OAS/OASAttendance.js
+++ frontend/nasdaily-app/src/Pages/OAS/OASAttendance.js
@@ -1,9 +1,120 @@
 import React, { Component } from 'react'
+import Header from '../../Components/Header.js';
+import Button from '../../Components/Common/Buttons/Button.js'
+import SummaryAttendanceOAS from '../../Components/OAS/SummaryAttendanceOAS.js';
+import '../OAS/OASAttendance.css'
+
 
 export class OASAttendance extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      fullName: '',
+      check: false,
+    };
+  }
+
+  handleSearch = (e) => {
+    e.preventDefault();
+    this.setState({
+      fullName: 'BELDEROL, KAYE CASSANDRA',
+      check: true,
+    });
+  };
+
+
   render() {
+    const { searchTerm, fullName, check } = this.state;
+
     return (
-      <div>OASAttendance</div>
+      <>
+        <div>
+          <Header username={"OAS"} onLogout={null} />
+        </div>
+
+        <div className='menuNav'>
+          <Button
+            style={"yellow-button"}
+            onClick={null}
+            name={"Offices"}
+          />
+          <Button
+            style={"red-button"}
+            onClick={null}
+            name={"Attendance"}
+          />
+          <Button
+            style={"yellow-button"}
+            onClick={null}
+            name={"Evaluation"}
+          />
+          <Button
+            style={"yellow-button"}
+            onClick={null}
+            name={"Grades"}
+          />
+          <Button
+            style={"yellow-button"}
+            onClick={null}
+            name={"Validation"}
+          />
+          <Button
+            style={"yellow-button"}
+            onClick={null}
+            name={"NAS Master List"}
+          />
+        </div>
+
+        <div className='row'>
+          <div className='heading'>
+            <p className='name'>{fullName}</p>
+            <form className='searchBar' onSubmit={this.handleSearch}>
+              <input
+                className='searchBarInput'
+                type='text'
+                placeholder='Search...'
+                value={searchTerm}
+                onChange={(e) => this.setState({ searchTerm: e.target.value })}
+              />
+              <button type='submit'>
+                <i className='material-icons search-icon'>search</i>
+              </button>
+            </form>
+          </div>
+
+          <div className='dropDownButtons'>
+            <label className='labelDropDown'>
+              SEMESTER:
+              <select>
+                <option value="semester">2nd Semester 2022-2023</option>
+              </select>
+            </label>
+            <label className='labelDropDown'>
+              MONTH:
+              <select>
+                <option value="month">January</option>
+                <option value="month">February</option>
+                <option value="month">March</option>
+                <option value="month">April</option>
+                <option value="month">May</option>
+              </select>
+            </label>
+          </div>
+
+          <div className='data'>
+            <SummaryAttendanceOAS
+              check={check}
+              totalLates={2}
+              unexcusedAbsent={1}
+              excusedAbsent={1}
+              tenMinLate={1}
+              fortyFiveMinLate={1}
+              ftp={1}
+            />
+          </div>
+        </div>
+      </>
     )
   }
 }
