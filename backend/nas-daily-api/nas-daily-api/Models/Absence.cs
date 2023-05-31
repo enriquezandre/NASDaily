789195a6999fbcbbd4573f2ec8fc5ca342127056
Kathlen Claire Procorato <kathlenclaire.procorato@cit.edu>
2023-05-31 23:57:27 +0800

    Save changes to Absence model

--- backend/nas-daily-api/nas-daily-api/Models/Absence.cs
+++ backend/nas-daily-api/nas-daily-api/Models/Absence.cs
@@ -3,10 +3,11 @@
     public class Absence
     {
         public string? AbsenceId { get; set; }
-        public string? Excused { get; set; }
-        public string? Unexcused { get; set; }
+        public int? Excused { get; set; }
+        public int? Unexcused { get; set; }
         public string? Ftp { get; set; }
         public string? LateOver10mins { get; set; }
         public string? LateOver45mins { get; set; }
+        public NAS? Nas { get; set; } = new NAS();
     }
 }
