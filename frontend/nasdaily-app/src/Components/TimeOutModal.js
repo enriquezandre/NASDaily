71ac8776d6565c83bc4ddf31fb0826c1becee5ab
Andre Enriquez <andre.enriquez@cit.edu>
2023-06-01 03:30:10 +0800

    Convert some components to class

--- frontend/nasdaily-app/src/Components/TimeOutModal.js
+++ frontend/nasdaily-app/src/Components/TimeOutModal.js
@@ -1,58 +1,61 @@
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
-import './TimeOutModal.css'
+import './TimeOutModal.css';
 
-function TimeOutModal(props) {
-  return (
-    <Modal
-        show={props.show}
-        onHide={props.onHide}
+class TimeOutModal extends React.Component {
+  render() {
+    return (
+      <Modal
+        show={this.props.show}
+        onHide={this.props.onHide}
         size="lg"
         aria-labelledby="contained-modal-title-vcenter"
         centered
-    >
-      <div className='timeoutmodal-container'>
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            NAS Daily Activity
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Activities Done</Form.Label>
-              <Form.Control as="textarea" rows={3} className="text-area"/>
-            </Form.Group>
-          </Form>
-          <Form>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Skills Learned</Form.Label>
-              <Form.Control as="textarea" rows={3} className="text-area"/>
-            </Form.Group>
-          </Form>
-          <Form>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Values Learned</Form.Label>
-              <Form.Control as="textarea" rows={3} className="text-area"/>
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={props.onHide} className='timeoutmodal-btn'>Done</Button>
-        </Modal.Footer>
+      >
+        <div className='timeoutmodal-container'>
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              NAS Daily Activity
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Activities Done</Form.Label>
+                <Form.Control as="textarea" rows={3} className="text-area" />
+              </Form.Group>
+            </Form>
+            <Form>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Skills Learned</Form.Label>
+                <Form.Control as="textarea" rows={3} className="text-area" />
+              </Form.Group>
+            </Form>
+            <Form>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Values Learned</Form.Label>
+                <Form.Control as="textarea" rows={3} className="text-area" />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.props.onHide} className='timeoutmodal-btn'>Done</Button>
+          </Modal.Footer>
         </div>
       </Modal>
-  );
+    );
+  }
 }
 
 export default TimeOutModal;
--- frontend/nasdaily-app/src/Components/Welcome.js
+++ frontend/nasdaily-app/src/Components/Welcome.js
@@ -1,43 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './Welcome.css';
 
-function Welcome() {
-  const [currentDate, setCurrentDate] = useState('');
-  const [currentTime, setCurrentTime] = useState('');
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const updateDateTime = () => {
-      const now = new Date();
-      const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-      setCurrentDate(now.toLocaleDateString(undefined, options));
-      setCurrentTime(now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentDate: '',
+      currentTime: '',
+      user: null
     };
+  }
 
-    const fetchUserData = async () => {
-      try {
-        const response = await fetch('https://localhost:7047/api/nas');
-        if (response.ok) {
-          const userData = await response.json();
-          setUser(userData[0]);
-        } else {
-          throw new Error('Failed to fetch user data');
-        }
-      } catch (error) {
-        console.error('Error:', error);
+  componentDidMount() {
+    this.updateDateTime();
+    this.fetchUserData();
+
+    this.timer = setInterval(this.updateDateTime, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  updateDateTime = () => {
+    const now = new Date();
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    this.setState({
+      currentDate: now.toLocaleDateString(undefined, options),
+      currentTime: now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+    });
+  };
+
+  fetchUserData = async () => {
+    try {
+      const response = await fetch('https://localhost:7047/api/nas');
+      if (response.ok) {
+        const userData = await response.json();
+        this.setState({ user: userData[0] });
+      } else {
+        throw new Error('Failed to fetch user data');
       }
-    };
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
-    updateDateTime();
-    fetchUserData();
-
-    const timer = setInterval(updateDateTime, 1000);
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
-  const getFirstName = (fullName) => {
+  getFirstName = (fullName) => {
     const names = fullName.split(' ');
     if (names.length > 0) {
       return names[0];
@@ -45,17 +53,20 @@ function Welcome() {
     return '';
   };
 
-  const firstName = user ? getFirstName(user.name) : '';
+  render() {
+    const { currentDate, currentTime, user } = this.state;
+    const firstName = user ? this.getFirstName(user.name) : '';
 
-  return (
-    <div className="welcome-container">
-      <p className="welcome-text">HELLO, {firstName}!</p>
-      <div className="date-time">
-        <p className="date">{currentDate}</p>
-        <p className="time">{currentTime}</p>
+    return (
+      <div className="welcome-container">
+        <p className="welcome-text">HELLO, {firstName}!</p>
+        <div className="date-time">
+          <p className="date">{currentDate}</p>
+          <p className="time">{currentTime}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Welcome;
